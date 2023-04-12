a186215066d0786dc1ceb287d22bcfff41c513dd
jeongSWsw <jeongsw34@gmail.com>
2023-04-12 16:53:52 +0900

    BJ1152

--- ex06_string/src/ex06_string/BJ1152.java
+++ ex06_string/src/ex06_string/BJ1152.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class BJ1152 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String line = sc.nextLine();
+		String line = sc.nextLine().trim();
+		line = line.trim();
 		String[] arr = line.split(" ");
-		System.out.println(arr.length);
+
+		if (arr.length == 1 && arr[0].equals("")) {
+			System.out.println(0);
+		} else {
+			System.out.println(arr.length);
+		}
 	}
 }
