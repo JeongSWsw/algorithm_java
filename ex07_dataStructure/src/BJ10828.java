d0d43923334faa3b82b8013352de315dbe8b712f
JeongSWsw <jeongsw34@gmail.com>
2023-04-13 22:45:30 +0900

    BJ10828_2

--- ex07_dataStructure/src/BJ10828.java
+++ ex07_dataStructure/src/BJ10828.java
@@ -1,54 +1,69 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.util.Vector;
 
 public class BJ10828 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+	public static void main(String[] args) throws Exception {
+//      Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+		int num = Integer.parseInt(br.readLine());
 		Vector v = new Vector<>();
-		String str = "";
+//		String str[];
+		StringTokenizer str;
 		for (int i = 0; i < num; i++) {
-			str = sc.next();
-			switch (str) {
+//			if(i == num -1) {
+//				sc.nextLine();
+//			}
+//			System.out.println("num:"+i);
+			str = new StringTokenizer(br.readLine());
+			// str = sc.nextLine().split(" ");
+			switch (str.nextToken()) {
 			case "push": {
-				int x = sc.nextInt();
+				int x = Integer.parseInt(str.nextToken());
 				v.add(x);
 				break;
 			}
 			case "top": {
 				if (v.isEmpty())
-					System.out.println(-1);
+//					System.out.println(-1);
+					sb.append("-1").append("\n");
 				else
-					System.out.println(v.lastElement());
-				break;
+//					System.out.println(v.lastElement());
+					sb.append(v.lastElement()).append("\n");
+					break;
 			}
 			case "size": {
-				System.out.println(v.size());
+//				System.out.println(v.size());
+				sb.append(v.size()).append("\n");
 				break;
 			}
 			case "empty": {
 				if (v.isEmpty())
-					System.out.println(1);
+//					System.out.println(1);
+					sb.append("1").append("\n");
 				else
-					System.out.println(0);
+//					System.out.println(0);
+					sb.append("0").append("\n");
 				break;
 			}
 			case "pop": {
 				if (v.isEmpty())
-					System.out.println(-1);
+//					System.out.println(-1);
+					sb.append("-1").append("\n");
 				else {
-					System.out.println(v.lastElement());
-					v.removeElementAt(v.size()-1);
+//					System.out.println(v.lastElement());
+					sb.append(v.lastElement()).append("\n");
+					v.removeElementAt(v.size() - 1);
 				}
 				break;
 			}
-			default :{
-				str = "";
-				break;
-			}
-			
-
-			}
-		}
+			}// swich end
+				// sc.nextLine();
+		} // for end
+		System.out.println(sb);
 	}
 }
---
+++ ex07_dataStructure/src/BJ10828_1.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ10828_1 {
+    public static int[] stack;
+    public static int size=0;
+
+    public static void push(int x) {
+        stack[size] = x;
+        size++;
+    }
+
+    public static int pop() {
+        if(size == 0) return -1; //빈 스택일 경우
+
+        int num = stack[size-1]; //삭제할 요소
+        stack[size-1] = 0; //삭제
+        size--;
+
+        return num;
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        if(size == 0) return 1;
+        else return 0;
+    }
+
+    public static int top() {
+        if(size == 0) return -1;
+
+        return  stack[size-1];
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        stack = new int[N];
+
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            switch(st.nextToken()){
+
+            case "push":
+                push(Integer.parseInt(st.nextToken()));
+                break;
+            case "pop":
+                sb.append(pop()).append("\n");
+                break;
+            case "size":
+                sb.append(size()).append("\n");
+                break;
+            case "empty":
+                sb.append(empty()).append("\n");
+                break;
+            case "top":
+                sb.append(top()).append("\n");
+                break;
+            }
+
+        }
+        System.out.println(sb);
+    }
+}
