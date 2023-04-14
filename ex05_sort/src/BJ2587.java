fb16c432e77f1a041027a78d0e058b07ca14c3c5
jeongSWsw <jeongsw34@gmail.com>
2023-04-14 09:33:14 +0900

    BJ2587

---
+++ ex05_sort/src/BJ2587.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BJ2587 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int num = 5;
+		int[] arr = new int[num];
+		int sum = 0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for(int i = 0; i < num; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+		Arrays.sort(arr);
+		System.out.println(sum/5);
+		System.out.println(arr[2]);
+		
+	}
+}
